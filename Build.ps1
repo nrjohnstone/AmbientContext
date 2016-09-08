92c570bec478edf6273006f1a4ea265e899132bf
Nathan Johnstone <locotewz@gmail.com>
2016-09-08 10:27:48 +0200

    update build number to 1.0.0 for release

--- Build.ps1
+++ Build.ps1
@@ -118,7 +118,7 @@ foreach ($project in $projects) {
     Exec { & dotnet build $project.Item1 }
 }
 
-# Get Rivision
+# Get Revision
 Header " CHECK REVISION"
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
