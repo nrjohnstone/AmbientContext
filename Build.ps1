e2dbaa8708e66a1ab6db334debd81638676cd0c8
Nathan Johnstone <locotewz@gmail.com>
2016-09-09 06:48:08 +0200

    add some conditional logic to versioning and update project.json

--- Build.ps1
+++ Build.ps1
@@ -120,9 +120,16 @@ foreach ($project in $projects) {
 
 # Get Revision
 Header " CHECK REVISION"
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-"REVISION: " + $revision
+if  ($env:APPVEYOR_REPO_BRANCH -ne "Release")
+{
+    $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+    $revision = "-CI{0:D4}" -f [convert]::ToInt32($revision, 10)
+    "REVISION: " + $revision
+    Exec {
+        & ./tools/dnv --avpat "{{Same}}.{{Same}}.{{Same}}-pre{{UtcNow}}" --read .\src\AmbientContext\project.json --what av --verbose --write .\src\AmbientContext\project.json
+    }
+}
+
 
 # Run tests
 foreach ($project in $projects) {
